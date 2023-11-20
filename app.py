7a56b99a879573faa5cffff6ddf6dcd5237d99f1
bbmani <balajibharatwajmanikandan@gmail.com>
2023-11-19 20:50:22 -0500

    Successful App - Boilerplate

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template, request, jsonify
+import rstr as r
 
 app = Flask(__name__)
 
+def generate_password():
+    output = '{0}-{1}@{2}'.format(
+        r.domainsafe(5,10),
+        r.domainsafe(5,10),
+        r.domainsafe(5)
+    )
+    return output
+
+def main():
+    return_value = generate_password()
+    return return_value
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method=='POST':
-        # customInput = request.form['customInput']
-        customInput = "Your New Password is aabbccssdddd"
+        customInput = main()
         return jsonify({'output': customInput})
 
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=6969)
